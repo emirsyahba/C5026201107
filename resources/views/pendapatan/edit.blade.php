7aa5e8fd79cd5bfa74564a05bb5054880596a70b
Emirsyah Bayu Aji <emirsyah.205026@mhs.its.ac.id>
2021-12-02 15:34:45 +0700

    Kuliah 2 Des 2021

--- resources/views/pendapatan/edit.blade.php
+++ resources/views/pendapatan/edit.blade.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Kuliah Pertemuan 13</title>
+    <title>Kuliah Pertemuan 13</title>
 </head>
+
 <body>
 
-	<h2>Emirsyah Bayu Aji</h2>
-	<h3>Data Pendapatan</h3>
+    <h2>Emirsyah Bayu Aji</h2>
+    <h3>Data Pendapatan</h3>
 
-	<a href="/pendapatan"> Kembali</a>
+    <a href="/pendapatan"> Kembali</a>
 
-	<br/>
-	<br/>
+    <br />
+    <br />
 
-	@foreach($pendapatan as $p)
-	<form action="/pendapatan/update" method="post">
-		{{ csrf_field() }}
-		<input type="hidden" name="id" value="{{ $p->ID }}"> <br/>
-		IDPegawai <input type="number" name="idpegawai" required="required" value="{{ $p->IDPegawai }}"> <br/>
-		Bulan <input type="number" name="bulan" required="required" value="{{ $p->Bulan }}"> <br/>
-		Tahun <input type="text" name="tahun" required="required" value="{{ $p->Tahun }}"> <br/>
-		Gaji <input type="number" name="gaji" required="required">{{ $p->Gaji }} <br/>
-        Tunjangan <input type="number" name="tunjangan" required="required">{{ $p->Tunjangan }} <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
-	@endforeach
+    @foreach ($pendapatan as $p)
+        <form action="/pendapatan/update" method="post">
+            {{ csrf_field() }}
+            <input type="hidden" name="id" value="{{ $p->ID }}"> <br />
+            IDPegawai <input type="number" name="idpegawai" required="required" value="{{ $p->IDPegawai }}"> <br />
+            Bulan <input type="number" name="bulan" required="required" value="{{ $p->Bulan }}"> <br />
+            Tahun <input type="text" name="tahun" required="required" value="{{ $p->Tahun }}"> <br />
+            Gaji <input type="number" name="gaji" required="required">{{ $p->Gaji }} <br />
+            Tunjangan <input type="number" name="tunjangan" required="required">{{ $p->Tunjangan }} <br />
+            <input type="submit" value="Simpan Data">
+        </form>
+    @endforeach
 
 
 </body>
+
 </html>
